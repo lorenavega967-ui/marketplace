b14ceddacde894e396f32503b8ea23214462662d
barc1023 <arodriguezbryan57@gmail.com>
2026-06-28 17:38:45 -0500

    Agregar middleware de roles, seeders de admin y dueno, navegacion en bootstrap

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth', 'role:admin'])->get('/test-admin', function () {
+    return 'Solo el admin ve esto';
+});
+
 require __DIR__.'/auth.php';
