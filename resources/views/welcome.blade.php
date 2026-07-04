901552418b91ae90446298d7b62941a3d8e17a98
Linda Zaldana <lindazaldana.edu@gmail.com>
2026-07-04 18:04:59 -0500

    Inicio modulo panel emprendedor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,10 @@
                 <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
-                            href="{{ url('/dashboard') }}"
+                            href="{{ url('/perfil') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
-                            Dashboard
+                            Mi Perfil
                         </a>
                     @else
                         <a
@@ -38,13 +38,6 @@
                             Log in
                         </a>
 
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
-                            </a>
-                        @endif
                     @endauth
                 </nav>
             @endif
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerfilController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/perfil', [PerfilController::class, 'index'])->name('perfil');
+Route::get('/dashboard', [PerfilController::class, 'index'])->name('dashboard');
